d8a00d16de22fde2fc9dcee3ee7c95101ffbe6ed
GabRab <gabriel.09011@outlook.com>
2026-05-27 22:44:50 +0200

    updated logs.php

--- logs.php
+++ logs.php
@@ -1,7 +1,44 @@
 <?php
-//HELLO, this is a place where I migrated most of the logs from index.php to make it more comprehensible because they were taking up too much space.
-//This is kinda like an easter egg where it just doesn't even get used anywhere and even if you somehow manage to find it then nothing gets displayed to the user.
+//HELLO, this is a place where I migrated most of the logs from index.php and README.md because they were getting too annoying.
+//This is kinda like an easter egg where it just doesn't even get used anywhere and even if you somehow manage to find it then nothing gets displayed for the user since its all comments.
+//anyways, here's some logs of what I did when. I'm not sure how accurate they are, but the dates at least say which days I worked on this.
 
+//1.4. večer jsem začal pracovat na tomhle projektu, zatím jenom bojování s css a trochu rozpracování classy do php
+
+//2.4. celý den jsem pracoval na projektu do WEA, rozpracoval jsem přihlašování a přidal trochu css
+
+//3.4. moc jsem toho neudělal, jenom další bojování s css, přihlašování je funkční ale měl jsem super nápad a hodil všechno na jednu stránku, takže budu muset bojovat s css a dalšími věcmi.
+
+//4.4. další bojování s css, já prohrávám... trochu jsem popřeházel věci, ale budu se muset vrátit k css později. Taky jsem stihl udělat odhlášení.
+
+//5.4. zase další bojování s css, někam se dostávám ale je to hrozně pomalé a radši bych aspoň udělal kostru dnež se vrátím k mučení s css (zas tak velký masochista nejsem). Celý den pracoju na fotkách uživatele, ale nikam to nejde protože to furt blbne divně. Rozhodl jsem se že budu dělat e-shop.
+
+//06.04 
+// I woke up and immediately started working, at least the backend. Css sometime later... Hopefully after I finally make it work.
+//I added a bunch of php, everything after the user functions today. That means products, ratings and that's about it...
+
+//13.04
+//I'm back! I took a break because I was falling apart thinking too much about how it wasn't working so I focused on school and did some reading over the weekend so I didn't have time to do this sadly...
+//Today I want to make this thing work, because the stuff I added last time is a mess and just straight up doesn't work.
+//Ok, so instead of going through the depression that is dealing with products, I instead spent... 3 hours (maybe?) making pictures.php and adding stuff to it. It works as well and I improved some more stuff on header.php
+//Why am I putting every function into header.php? I... dunno. I kinda don't want to stop despite it being a clearly good idea from the amount of lines its accumulating.
+
+//15.04 -
+//brought to git today. I'll see where I left off and try to continue. Please excuse the ramblings of previous dates... I'm tired despite not even doing much today.
+//Last time I added pictures.php and made it all work, today I guess I'm going to expand on that a little bit.
+//Zapomněl jsem že děláme fotogalerii, takže můžu zahodit většinu těch věcí co tam jsou. Zatím jenom v php naštěstí.
+//Css bude dělat Martin, protože mi vůbec nejde.
+//Spravil jsem funkce v php, takze uz funguje vsechno co tam zatim máme. (až na produkty ale to stejně zahodím)
+//17.04 -
+//added functions for adding images, getting specifically your images, editing and removing (untested)
+//18.04 -
+//I changed pictures.php into index.php because its useless otherwise, I also included init.php in header.phtml because it would've just been useless like that too...
+//I might add posts to this too, but that would be more work. I really am making r34 aren't I?
+//sooo... basic restructuring due to elementary mistakes and a few untested functions(also untested)... yay. I still have to reach the object quota of 3 though... I want to cry.
+//19.04. -
+//fixed some stuff I added last time, general fixes in html, functions, ...
+//I expanded on some stuff and removed some stuff
+//I also changed some stuff so that it is a bit more comprehensible (header.phtml is still a mess tho ;-;)
 
 //18.04.2026 I changed images.php into index.php because its useless otherwise, I also included init.php in header.phtml because it would've just been useless like that too...
 //I might add posts to this too, but that would be more work. I really am making r34 aren't I?
@@ -126,4 +163,5 @@
 //26.05.2026 More adjusting of css... almost looks usable, yet there still is work to be done. Mainly just the frontend though.
 //27.06.2026 Making image.php more presentable, more little css changes, I hate css so much.  Also added some php objects to make tags a bit better.
 //What's next? Dividing images into pages could be done, but thats kinda unnecessary. Make pretty, no, pls no more css. Fix header.php, It's not broken, just feels ugly. 
+//fixed header.php, though there's some jquery stuff that I'm not touching.
 ?>
